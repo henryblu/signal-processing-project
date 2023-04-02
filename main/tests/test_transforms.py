fe6f260fef213a222fa6957329eaec66be48ff70
henry <henry.s.blue@gmail.com>
2023-04-02 22:21:58 +0300

    started testing functions but havn't yet implemented them

---
+++ main/tests/test_transforms.py
@@ -0,0 +1,8 @@
+from scipy import fftpack
+import numpy as np
+
+def regular_fourier_transformtest(audio_data):
+    ''' this function tests the discrete fourier transform function using teh ecipy fourier transform function
+    '''
+    pass
+
