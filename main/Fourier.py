fc0a65b9585b216c3a97b7dd4344f87d9e5f1c1c
henry <henry.s.blue@gmail.com>
2023-04-15 20:29:00 +0300

    it tured out that once I'd implementing testing functionality, 3 of the functions didn't work as intended. So I fixed them.,

--- main/Fourier.py
+++ main/Fourier.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy import fftpack
 
 
-def discrete_fourier_transform(audio_data):
+def regular_fourier_transform(audio_data):
     ''' this function performs a regualr fourier transform on a given audio data
     '''
     N = len(audio_data)
@@ -13,40 +13,47 @@ def discrete_fourier_transform(audio_data):
             fourier_transform[k] += audio_data[n]*np.exp(-2j*np.pi*k*n/N)
     return fourier_transform
 
+
 def fast_fourier_transform(audio_data):
-    ''' this function performs a fastg fourier transform on a given audio data
+    ''' This function performs a fast fourier transform on a given audio data it only workd for audio data with a length that is a power of 2
     '''
-    N = len(audio_data)
+    N = int(len(audio_data))
+
     if N <= 1:
         return audio_data
-    even = fast_fourier_transform(audio_data[::2])
-    odd = fast_fourier_transform(audio_data[1::2])
-    fourier_transform = [0]*N
-    for k in range(N//2):
-        fourier_transform[k] = even[k] + np.exp(-2j*np.pi*k/N)*odd[k]
-        fourier_transform[k+N//2] = even[k] - np.exp(-2j*np.pi*k/N)*odd[k]
-
-    # this isnt working yet and I cant seem to figure out whats wrong. will try again later
-    # fourier_transform = fourier_transform[:len(fourier_transform)//2]
+    elif N % 2 != 0: 
+       raise ValueError("size of audio_data must be a power of 2")
     
-    return fourier_transform
+    even = fast_fourier_transform(audio_data[:N:2])
+    odd = fast_fourier_transform(audio_data[1:N:2])
+
+    factor = np.exp(-2j*np.pi*np.arange(N)/N)
+    
+    return np.concatenate([even + factor[:N//2]*odd, even + factor[N//2:]*odd])
+
+
+
+
+def inverse_regular_fourier_transform(fourier_transform):
+    ''' this function performs an inverse regular fourier transform on a given fourier transform
+    '''
+
+    N = len(fourier_transform)
+    inverse_fourier_transform = np.zeros(N, dtype=complex)
+    for k in range(N):
+        for n in range(N):
+            inverse_fourier_transform[k] += fourier_transform[n]*np.exp(2j*np.pi*k*n/N)
+
+    return inverse_fourier_transform/N
+
 
 def inverse_fast_fourier_transform(fourier_transform):
     ''' this function performs an inverse fast fourier transform on a given fourier transform
     '''
-    #new_sound_wave = fftpack.ifft(fourier_transform)
-    N = len(fourier_transform)
-    if N <= 1:
-        return fourier_transform
-    even = inverse_fast_fourier_transform(fourier_transform[::2])
-    odd = inverse_fast_fourier_transform(fourier_transform[1::2])
-    audio_data = [0]*N
-    for k in range(N//2):
-        audio_data[k] = even[k] + np.exp(2j*np.pi*k/N)*odd[k]
-        audio_data[k+N//2] = even[k] - np.exp(2j*np.pi*k/N)*odd[k]
-    return audio_data
-
-    #return new_sound_wave
-
+    N = int(len(fourier_transform))
+    n = np.arange(N)
+    k = n.reshape((N, 1))
+    M = np.exp(2j * np.pi * k * n / N)
+    return np.dot(M, fourier_transform)/N
 
     
