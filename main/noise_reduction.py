711fdb74f5e51d3de221f692b9cd1a530dae89aa
henry <henry.s.blue@gmail.com>
2023-04-16 21:37:53 +0300

    attempt number 2 of noise reduction

--- main/noise_reduction.py
+++ main/noise_reduction.py
@@ -3,13 +3,12 @@ import numpy as np
 from scipy import fftpack
 
 def noise_reduction(fourier_transform, threadshold_level):
-    '''this function performs a noise reduction on a given fourier transform depended on the given threashold
+    '''this function performs a noise reduction on a given fourier transform depended on the given threashold. It dosnt work yet
     '''
-    # first we clean up the fourier transform to have only frequencies smaller then 20000 and positive 
-
-    # then we calculate the mean and standard deviation of the fourier transform
+    # first we calculate the mean and the standard deviation of the fourier transform
     mean = np.mean(fourier_transform)
     std = np.std(fourier_transform)
+
     # then we calculate the threadshold
     try:
         if (threadshold_level < 0):
@@ -18,10 +17,6 @@ def noise_reduction(fourier_transform, threadshold_level):
     except TypeError:
         print('threadshold level must be a number')
         exit
-    
     threadshold = mean - (4-threadshold_level)*std
-    # then we set all the values in the fourier transform that are below the threadshold to 0
     
-    
-    # then we return the fourier transform
     return fourier_transform
