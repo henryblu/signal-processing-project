f5e996a590c73ca5c1fb8f50a4288e35f12d8fc9
henry <henry.s.blue@gmail.com>
2023-04-16 21:36:39 +0300

    added a sample wave function

--- main/data_processing.py
+++ main/data_processing.py
@@ -2,7 +2,28 @@ import numpy as np
 from scipy.io import wavfile as wav
 
 
+def test_wave_generation(duration = 1, sample_rate = 512):
+    ''' this function generates a composite wavfe of three random waves of a given sample rate and duration
+    '''
+    print()
+    print("no file specified, generating random composite wave of 3 frequencies")
 
+    # generate a sine wave with a randome frequency
+    frequency_list = [np.random.randint(1, sample_rate), np.random.randint(1, sample_rate), np.random.randint(1, sample_rate)]
+    signal_1 = np.sin(2*np.pi*frequency_list[0]*np.arange(sample_rate*duration)/sample_rate)
+    signal_2 = np.sin(2*np.pi*frequency_list[1]*np.arange(sample_rate*duration)/sample_rate)
+    signal_3 = np.sin(2*np.pi*frequency_list[2]*np.arange(sample_rate*duration)/sample_rate)
+    composite_signal = signal_1 + signal_2 + signal_3
+
+    print()
+    print('wave specifications: ')
+    print('    duration of wave: ' + str(duration) + ' seconds')
+    print('    sample rate: ' + str(sample_rate) + ' samples per second')
+
+    print("    frequencies of composite signal (in hz): " + ", ".join([str(x) + "hz" for x in frequency_list]))
+    print()
+
+    return (sample_rate, composite_signal)
 
 def get_data(audio_file_path):
     ''' this function is used to convert the audiofile to a numpy array
