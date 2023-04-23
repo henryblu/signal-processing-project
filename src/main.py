3463ef7877eea5591b56f4b4137e9eace3efb39a
henry <henry.s.blue@gmail.com>
2023-04-23 22:29:58 +0300

    cleaning main

--- src/main.py
+++ src/main.py
@@ -12,25 +12,24 @@ def print_help():
         Currently, this program can be used to test the functionality of two types of fourier and inverse fourier transforms.
 
         current limitations:
-        - the program can only read .wav files
+        - the program can only read and write to .wav files
         - the program can only write to .csv files
-        - the flag --details which prints a detailed output of the algrithm including time taken and memory taken is not yet implemented (if you want to see time taken and memory taken for a test wave you can use the --test_with_details flag)
-        - the flag --threadshold which specifies the threadshold level for the noise reduction, default is 0 is not yet implemented (as the noise reduction algorithm is not yet implemented)
+        - unit tests must be run one by one
+
         """
     ) 
 
 def flags_finder(performance_test = 0, ):
-    # Parse command line arguments 
+    ''' this function is used to find all the flags that have been set.
+    '''
     parser = argparse.ArgumentParser()
-    parser.add_argument("-H", "--helper", help="prints a more detailed help message", action="store_true")
-    parser.add_argument("-i", "--input", help="specifies the .wav sound wave file to be read. Default is sound wave provided")
-    parser.add_argument("-o", "--output", help="specifies the file to be written to default is output.csv")
-    #parser.add_argument("-d", "--details", help="prints the details of the algrithm including time taken and memory taken", action="store_true")
-    parser.add_argument("-fft", "--fast_fourier_transform", help="performs a fast fourier transform on the data", action="store_true")
-    parser.add_argument("-rft", "--regular_fourier_transform", help="performs a regular fourier transform on the data", action="store_true")
-    parser.add_argument("-p", "--performance_test", help="prints the details of the test suite including time taken and memory taken. You may input a positive integer to specify number of tests you want to run", type=int, default=0)
-    parser.add_argument("-v", "--verbose", help="prints the details what is happening in the program", action="store_true")
-    #parser.add_argument("-th", "--threadshold", help="specifies the threadshold level for the noise reduction, default is 0", type=int, default=0)
+    parser.add_argument("-H", "--helper", help="prints a help message on the current state of the project", action="store_true")
+    parser.add_argument("-i", "--input", help="specifies the .wav sound wave file to be read. Default is randomly generated sound wave")
+    parser.add_argument("-o", "--output", help="specifies the file to be written to default is ./data/output.csv")
+    parser.add_argument("-v", "--verbose", help="prints the details of the algrithm including time taken and memory taken", action="store_true")
+    parser.add_argument("-r", "--regular_fourier_transform", help="specifies the regular fourier transform to be used", action="store_true")
+    parser.add_argument("-f", "--fast_fourier_transform", help="specifies the fast fourier transform to be used", action="store_true")
+    parser.add_argument("-t", "--performance_test", help="specifies the number of samples to be used in the performance test. Default is 0", type=int, default=0)
     args = parser.parse_args()
 
     if args.helper:
@@ -41,7 +40,8 @@ def flags_finder(performance_test = 0, ):
 
 
 def main():
-    # first we look for all the flags that have been set 
+    ''' This function is the main function of the program it is used to call all the other functions
+    '''
     flags = flags_finder()
 
     if flags.performance_test != 0:
