8281285dc340f568052d0dc0433e470dc24a6ab1
henry <henry.s.blue@gmail.com>
2023-04-28 15:15:10 +0300

    linting

--- src/services/noise_reduction.py
+++ src/services/noise_reduction.py
@@ -1,20 +1,15 @@
-import numpy as np
 
 def noise_reduction(fourier_transform, threadshold_level):
-    '''this function performs a noise reduction on a given fourier transform depended on the given threashold. It dosnt work yet
-    '''
-    # first we calculate the mean and the standard deviation of the fourier transform
-    mean = np.mean(fourier_transform)
-    std = np.std(fourier_transform)
+    '''this function performs a noise reduction on a given fourier transform depended 
+    on the given threashold. It dosnt work yet
 
-    # then we calculate the threadshold
-    try:
-        if (threadshold_level < 0):
-            print('threadshold level must be a non negitive integer')
-            exit
-    except TypeError:
-        print('threadshold level must be a number')
-        exit
-    threadshold = mean - (4-threadshold_level)*std
-    
+    Args:
+        fourier_transform (np.array): the fourier transform to be noise reduced 
+        threadshold_level (int): the level of the threadshold
+
+    '''
+    N = len(fourier_transform)
+    for k in range(N):
+        if abs(fourier_transform[k]) < threadshold_level:
+            fourier_transform[k] = 0
     return fourier_transform
