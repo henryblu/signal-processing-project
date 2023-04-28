1df3d9d4f64d83cdc91c0482cc42b1d68d1c6941
henry <henry.s.blue@gmail.com>
2023-04-28 15:26:14 +0300

    more linting

--- src/services/data_processing.py
+++ src/services/data_processing.py
@@ -3,13 +3,20 @@ from scipy.io import wavfile as wav
 
 def input_checker(file, verbose = False):
     ''' this function is used to check if the input is valid
+
+    Args:
+        file (str): the path to the input file
+        verbose (bool): if true the function will print out the wave specifications
+    
+    Returns:
+        int: the sample rate of the audio data
+        np.array: the audio data
     '''
-    if file != None:
+    if file is not None:
         if file[-4:] != ".wav":
             print("Invalid input file type")
-            exit
-        else:
-            sample_rate, composite_signal = get_data(file)
+            return None, None
+        sample_rate, composite_signal = get_data(file)
     else:
         sample_rate, composite_signal = test_wave_generation(verbose = verbose)
 
@@ -19,55 +26,75 @@ def test_wave_generation(duration = 1, sample_rate = 512, verbose = False):
     ''' 
     this function generates a composite wavfe of three random waves of a given 
     sample rate and duration
+
+    Args:
+        duration (int): the duration of the wave in seconds
+        sample_rate (int): the sample rate of the wave
+        verbose (bool): if true the function will print out the wave specifications
+    
+    Returns:
+        int: the sample rate of the audio data
+        np.array: the audio data
     '''
     if verbose:
         print()
         print("no file specified, generating random composite wave of 3 frequencies")
 
-    # generate a sine wave with a randome frequency
-    frequency_list = [np.random.randint(1, sample_rate), np.random.randint(1, sample_rate), np.random.randint(1, sample_rate)]
+    frequency_list = [np.random.randint(1, sample_rate), np.random.randint(1, sample_rate),
+                      np.random.randint(1, sample_rate)]
     signal_1 = np.sin(2*np.pi*frequency_list[0]*np.arange(sample_rate*duration)/sample_rate)
     signal_2 = np.sin(2*np.pi*frequency_list[1]*np.arange(sample_rate*duration)/sample_rate)
     signal_3 = np.sin(2*np.pi*frequency_list[2]*np.arange(sample_rate*duration)/sample_rate)
     composite_signal = signal_1 + signal_2 + signal_3
-    
+
     if verbose:
         print()
         print('wave specifications: ')
         print('    duration of wave: ' + str(duration) + ' seconds')
         print('    sample rate: ' + str(sample_rate) + ' samples per second')
-
-        print("    frequencies of composite signal (in hz): " + ", ".join([str(x) + "hz" for x in frequency_list]))
+        print("    frequencies of composite signal (in hz): "
+                + ", ".join([str(x) + "hz" for x in frequency_list]))
         print()
-    
+
     return (sample_rate, composite_signal)
 
 def get_data(audio_file_path):
     ''' this function is used to convert the audiofile to a numpy array
+
+    Args:
+        audio_file_path (str): the path to the audio file
+
+    Returns:
+        int: the sample rate of the audio data
+        np.array: the audio data
     '''
     try:
         sample_rate, audio_data = wav.read(audio_file_path)
     except FileNotFoundError:
         print("input path not found")
-        exit
-    
+        return None, None
     return (sample_rate, np.array(audio_data))
 
 def output(sample_rate, new_sound_wave, output_file_path):
-    ''' this function is used to output the new sound wave to a file with default path: main\data\output.wav
+    ''' this function is used to output the new sound wave to a file. 
+    if no file is specified then the default file is used
+
+    Args:
+        sample_rate (int): the sample rate of the audio data
+        new_sound_wave (np.array): the audio data
     '''
-    # not working right now 
-    
-    if(output_file_path == None):
-        # if no file is specified then we use the default file this try exept is a workaround for the fact that the file is in a different location when running the tests and debugging
+    # not working right now
+    if not output_file_path:
+        # if no file is specified then we use the default file this try exept is
+        # a workaround for the fact that the file is in a different location when
+        # running the tests and debugging
         try:
-            wav.write("src\data\output.wav", sample_rate, new_sound_wave)
-        except FileNotFoundError: 
-            wav.write("..\data\output.wav", sample_rate, new_sound_wave)
+            wav.write(r"src\data\output.wav", sample_rate, new_sound_wave)
+        except FileNotFoundError:
+            wav.write(r"..\data\output.wav", sample_rate, new_sound_wave)
 
     else:
         try:
             wav.write(output_file_path, sample_rate, new_sound_wave)
-        except FileNotFoundError: 
+        except FileNotFoundError:
             print("output path not found")
-            exit
