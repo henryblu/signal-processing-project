73e3802dc7b48e04a11aa1e4c83d8dee6a40769c
henry <henry.s.blue@gmail.com>
2023-04-28 15:12:34 +0300

    linting transforms.py

--- src/services/transforms.py
+++ src/services/transforms.py
@@ -1,13 +1,23 @@
 import numpy as np
 
-def transform_caller(rft, fft, sample_rate, composite_signal):
+def transform_caller(rft, fft, composite_signal):
     '''  this function is used to call the appropreate transform 
+
+    Args:
+        rft (bool): if true the regular fourier transform will be used
+        fft (bool): if true the fast fourier transform will be used
+        sample_rate (int): the sample rate of the audio data
+        composite_signal (np.array): the audio data
+    
+    Returns:
+        np.array: the fourier transform of the audio data
+        np.array: the inverse fourier transform of the fourier transform
     '''
     if rft and fft:
         print("You may only select one fourier transform")
-        return
+        return None, None
 
-    elif rft:
+    if rft:
         fourier_transform = regular_fourier_transform(composite_signal)
         inverse = inverse_regular_fourier_transform(fourier_transform)
     elif fft:
@@ -18,11 +28,17 @@ def transform_caller(rft, fft, sample_rate, composite_signal):
         print("defaulting to fast fourier transform")
         fourier_transform = fast_fourier_transform(composite_signal)
         inverse = inverse_fast_fourier_transform(fourier_transform)
-    
     return fourier_transform, inverse
 
 def regular_fourier_transform(audio_data):
     ''' this function performs a regualr fourier transform on a given audio data
+
+    Args:
+        audio_data (np.array): the audio data to be transformed
+    
+    Returns:
+        np.array: the fourier transform of the audio data
+
     '''
     N = len(audio_data)
     fourier_transform = np.zeros(N, dtype=complex)
@@ -32,14 +48,22 @@ def regular_fourier_transform(audio_data):
     return fourier_transform
 
 def fast_fourier_transform(audio_data):
-    ''' This function performs a fast fourier transform on a given audio data it only workd for audio data with a length that is a power of 2
+    ''' This function performs a fast fourier transform on a given audio data it only 
+    worked for audio data with a length that is a power of 2
+
+    Args:
+        audio_data (np.array): the audio data to be transformed
+
+    Returns:
+        np.array: the fourier transform of the audio data
+
     '''
     N = int(len(audio_data))
     if N <= 1:
         return audio_data
-    elif N % 2 != 0: 
-       raise ValueError("size of audio_data must be a power of 2")
-    
+    if N % 2 != 0:
+        raise ValueError("size of audio_data must be a power of 2")
+
     even = fast_fourier_transform(audio_data[:N:2])
     odd = fast_fourier_transform(audio_data[1:N:2])
 
@@ -48,6 +72,12 @@ def fast_fourier_transform(audio_data):
 
 def inverse_regular_fourier_transform(fourier_transform):
     ''' this function performs an inverse regular fourier transform on a given fourier transform
+
+    Args:
+        fourier_transform (np.array): the fourier transform to be transformed
+
+    Returns:
+        np.array: the inverse fourier transform of the fourier transform
     '''
 
     N = len(fourier_transform)
@@ -60,11 +90,16 @@ def inverse_regular_fourier_transform(fourier_transform):
 
 def inverse_fast_fourier_transform(fourier_transform):
     ''' this function performs an inverse fast fourier transform on a given fourier transform
+
+    Args:
+        fourier_transform (np.array): the fourier transform to be transformed
+        
+    Returns:
+        np.array: the inverse fourier transform of the fourier transform
     '''
+
     N = int(len(fourier_transform))
     n = np.arange(N)
     k = n.reshape((N, 1))
     M = np.exp(2j * np.pi * k * n / N)
-    return np.dot(M, fourier_transform)/N
-
-    
+    return np.dot(M,fourier_transform)/N
