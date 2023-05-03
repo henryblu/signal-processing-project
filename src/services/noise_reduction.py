42f2a61c93e3c8a986751d05afe5de8a523179e7
henry <henry.s.blue@gmail.com>
2023-05-03 12:09:56 +0300

    I got the noise reduction to work

--- src/services/noise_reduction.py
+++ src/services/noise_reduction.py
@@ -1,14 +1,20 @@
+import numpy as np
 def noise_reduction(fourier_transform, threadshold_level):
-    """this function performs a noise reduction on a given fourier transform depended
-    on the given threashold. It dosnt work yet
+    """this function performs a simple noise reduction on a given fourier transform depended
+    on the given threashold.
 
     Args:
         fourier_transform (np.array): the fourier transform to be noise reduced
-        threadshold_level (int): the level of the threadshold
-
+        threadshold_level (int): the threashold is how many standard deviations below the mean we want to make the cut off point
+    
+    Returns:
+        np.array: the noise reduced fourier transform
     """
-    N = len(fourier_transform)
-    for k in range(N):
-        if abs(fourier_transform[k]) < threadshold_level:
-            fourier_transform[k] = 0
-    return fourier_transform
+    max_val = np.max(np.abs(fourier_transform))
+    threadshold = threadshold_level/100 * max_val
+    noise_reduced_transform = np.copy(fourier_transform)
+    noise_reduced_transform[np.abs(noise_reduced_transform) < threadshold] = 0
+    return noise_reduced_transform
+
+
+
