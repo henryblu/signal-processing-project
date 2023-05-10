d017d88c6e55cf9fb89a6b606a00716c9d17e77a
henry <henry.s.blue@gmail.com>
2023-05-10 11:30:48 +0300

    better testing for data_processing

--- src/tests/services/test_data_processing.py
+++ src/tests/services/test_data_processing.py
@@ -1,4 +1,5 @@
 import unittest
+import pytest
 import numpy as np
 from scipy.io import wavfile as wav
 from services.data_processing import AudioFileProcessing
@@ -6,6 +7,22 @@ from services.data_processing import AudioFileProcessing
 
 class TestAudioFileProcessing(unittest.TestCase):
     def setUp(self):
+
+        with pytest.raises(ValueError, match="input file must be a .wav file"):
+            self.audio_file_processing = AudioFileProcessing(
+            input_file=r"no_work"
+        )
+            
+        with pytest.raises(FileNotFoundError, match="input file not found"):
+            self.audio_file_processing = AudioFileProcessing(
+            input_file=r"no_work.wav"
+        )
+            
+        with pytest.raises(ValueError, match="input file is too long"):
+            self.audio_file_processing = AudioFileProcessing(
+            input_file=r"src/tests/Data/StarWars60.wav"
+        )
+            
         self.audio_file_processing = AudioFileProcessing(
             input_file=r"src/Data/StarWars3.wav", verbose=True
         )
@@ -19,20 +36,28 @@ class TestAudioFileProcessing(unittest.TestCase):
         self.assertIsInstance(self.audio_file_processing.get_sample_rate(), int)
 
     def test_data_triming(self):
-        self.audio_file_processing.data_triming()
         self.assertIsInstance(self.audio_file_processing.front_trim, int)
         self.assertIsInstance(self.audio_file_processing.back_trim, int)
 
 
     def test_output(self):
         # clear the output file located at src/Data/output.wav
-        wav.write(r"src/Data/StarWars3.wav", 0, np.zeros((1,)))
+        wav.write(r"src/tests/Data/test_output.wav", 0, np.zeros((1,)))
         sound_wave = self.audio_file_processing.get_audio_data()
-        self.audio_file_processing.output(sound_wave)
+        self.audio_file_processing.output(sound_wave, r"src/tests/Data/test_output.wav")
         np.allclose(
             self.audio_file_processing.get_audio_data(),
-            wav.read(r"src/Data/output.wav")[1],
+            wav.read(r"src/tests/Data/test_output.wav")[1],
         )
+
+
+        self.audio_file_processing = AudioFileProcessing(
+            input_file=r"src/Data/StarWars3.wav", verbose=True
+        )
+
+        with pytest.raises(FileNotFoundError, match="The specified output path does not exist."):
+            self.audio_file_processing.output(sound_wave, r"spc/sql/no_work/please.wav")
+            
 
     def tearDown(self):
         pass
