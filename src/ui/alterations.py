7df95d05e3082daa29c982cf7da9a93e5587388a
henry <henry.s.blue@gmail.com>
2023-05-10 09:52:35 +0300

    made the imput messages a bit more clear and changed it to match transform alterations

--- src/ui/alterations.py
+++ src/ui/alterations.py
@@ -1,4 +1,8 @@
-from services.transform_alternations import TransformAlterations
+from services.transform_alternations import (
+    high_pitch_reduction,
+    low_pitch_reduction,
+    noise_reduction,
+)
 
 
 def run_alterations(old_fourier_transform):
@@ -11,23 +15,6 @@ def run_alterations(old_fourier_transform):
     Returns:
         np.array: the altered fourier transform
     """
-
-    ta = TransformAlterations()
-
-    while True:
-        threadshold_level = input(
-            "Please enter the desired threadshold_level (0 to 100): "
-        )
-        try:
-            threadshold_level = int(threadshold_level)
-            if threadshold_level < 0 or threadshold_level > 100:
-                print("Invalid input, please try again")
-                print()
-            else:
-                break
-        except ValueError:
-            print("Invalid input, please try again")
-            print()
 
     while True:
         print("What type of wave alteration would you like to perform?")
@@ -42,19 +29,66 @@ def run_alterations(old_fourier_transform):
         )
 
         if user_input == "1":
-            altered_fourier_transform = ta.high_pitch_reduction(
-                old_fourier_transform, threadshold_level
+            while True:
+                try:
+                    max_frequency = int(
+                        input(
+                            "Please enter the desired maximum frequency in hz (0 to 20000): "
+                        )
+                    )
+                    if max_frequency < 0 or max_frequency > 20000:
+                        print("Invalid input, please try again")
+                        print()
+                    else:
+                        break
+                except ValueError:
+                    print("Invalid input, please try again")
+                    print()
+            altered_fourier_transform = high_pitch_reduction(
+                old_fourier_transform, max_frequency
             )
             break
         elif user_input == "2":
-            altered_fourier_transform = ta.low_pitch_reduction(
-                old_fourier_transform, threadshold_level
+            while True:
+                try:
+                    min_frequency = int(
+                        input(
+                            "Please enter the desired minimum frequency in hz (0 to 20000): "
+                        )
+                    )
+                    if min_frequency < 0 or min_frequency > 20000:
+                        print("Invalid input, please try again")
+                        print()
+                    else:
+                        break
+                except ValueError:
+                    print("Invalid input, please try again")
+                    print()
+
+            altered_fourier_transform = low_pitch_reduction(
+                old_fourier_transform, min_frequency
             )
             break
         elif user_input == "3":
-            altered_fourier_transform = ta.noise_reduction(
+            while True:
+                threadshold_level = input(
+                    "Please enter the desired threadshold_level (0 to 100): "
+                )
+                try:
+                    threadshold_level = int(threadshold_level)
+                    if threadshold_level < 0 or threadshold_level > 100:
+                        print("Invalid input, please try again")
+                        print()
+                    else:
+                        break
+                except ValueError:
+                    print("Invalid input, please try again")
+                    print()
+
+            altered_fourier_transform = noise_reduction(
                 old_fourier_transform, threadshold_level
             )
+
             break
         elif user_input == "4":
             altered_fourier_transform = old_fourier_transform
