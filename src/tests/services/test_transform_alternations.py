04bea5a5c0634e79b74295f25f8a891895315cb8
henry <henry.s.blue@gmail.com>
2023-05-13 15:49:16 +0300

    updating the transform alterations tests to function properly and improving coverage

--- src/tests/services/test_transform_alternations.py
+++ src/tests/services/test_transform_alternations.py
@@ -11,33 +11,29 @@ from services.transform_alternations import (
 
 class TestTransformAlternations(unittest.TestCase):
     def setUp(self):
-        self.sample_wave = SampleWave()
+        self.sample_wave = SampleWave(sample_rate=20000)
         self.transform = np.fft.fft(self.sample_wave.generate_composite_wave())
 
     def test_high_pitch_reduction(self):
-        high_pitch_reduction_audio_data = high_pitch_reduction(self.transform)
+        high_pitch_reduction_audio_data = high_pitch_reduction(self.transform, 20000)
         self.assertEqual(high_pitch_reduction_audio_data.shape, self.transform.shape)
         self.assertTrue(np.all(high_pitch_reduction_audio_data == self.transform))
 
-        high_pitch_reduction_audio_data = high_pitch_reduction(self.transform, 50)
+        high_pitch_reduction_audio_data = high_pitch_reduction(self.transform, 10000)
         self.assertEqual(high_pitch_reduction_audio_data.shape, self.transform.shape)
-        self.assertTrue(
-            high_pitch_reduction_audio_data[int((len(self.transform) / 2) + 1)] == 0
-        )
+        self.assertTrue(high_pitch_reduction_audio_data[10001] == 0)
 
     def test_low_pitch_reduction(self):
-        low_pitch_reduction_audio_data = low_pitch_reduction(self.transform)
+        low_pitch_reduction_audio_data = low_pitch_reduction(self.transform, 0)
         self.assertEqual(low_pitch_reduction_audio_data.shape, self.transform.shape)
         self.assertTrue(np.all(low_pitch_reduction_audio_data == self.transform))
 
-        low_pitch_reduction_audio_data = low_pitch_reduction(self.transform, 50)
+        low_pitch_reduction_audio_data = low_pitch_reduction(self.transform, 10000)
         self.assertEqual(low_pitch_reduction_audio_data.shape, self.transform.shape)
-        self.assertTrue(
-            low_pitch_reduction_audio_data[int((len(self.transform) / 2) - 1)] == 0
-        )
+        self.assertTrue(low_pitch_reduction_audio_data[9999] == 0)
 
     def test_noise_reduction(self):
-        noise_reduction_audio_data = noise_reduction(self.transform)
+        noise_reduction_audio_data = noise_reduction(self.transform, 0)
         self.assertEqual(noise_reduction_audio_data.shape, self.transform.shape)
         self.assertTrue(np.all(noise_reduction_audio_data == self.transform))
 
